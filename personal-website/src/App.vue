99fe7f0b3bdf680e3e3132378d8fed612220f71f
DevilAlive <tony3864650@yahoo.com.tw>
2019-03-29 06:59:15 +0800

    Finished the navbar and carousel.

---
+++ personal-website/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Nav/>
+    <div data-spy="scroll" data-target="#navbarTarget" data-offset="0">
+      <Carousel/>
+      <div id="VueLogo" class="bg-warning d-flex align-items-center justify-content-center" style="height: 60vh;">
+        <img alt="Vue logo" src="./assets/logo.png">
+      </div>
+			<HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+    <br>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/Nav.vue";
+import Carousel from "./components/Carousel.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    Nav,
+    Carousel
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ personal-website/src/components/Nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav id="navbarTarget" class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+    <div class="container">
+      <a href="#" class="navbar-brand">SmallMO</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav justify-content-between">
+          <li class="nav-item" :key="navName.name" v-for="navName in navNames">
+            <a :href="'#'+navName.name" class="nav-link">{{ navName.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data: () => {
+    return {
+      navNames: [{ name: "Carousel" }, { name: "VueLogo" }, { name: "helloWorld1" }, { name: "helloWorld2" }, { name: "helloWorld3" }, { name: "helloWorld4" }]
+    };
+  }
+};
+</script>
